8ca11fc5b0c4debee8c289b10fc141044a932320
lovelymuose2010 <90010046+lovelymuose2010@users.noreply.github.com>
2021-09-03 06:14:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Em à 👉🏻👈🏻',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Zỏi thì bém zô đây nhóc!!!',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Ỏ em thích anh Thắng nhìuuuuuuuu lémmmmmmm <33',
+    btnNo: ' Hứ 😏 ghét cái mặt!!!!!',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Gửi cho teo ikkk hihi <3',
+    reply: 'Ỏoooooo em iu anh Thắng nhất trần đời này nun <3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messDesc: 'Anh chin nhũi zì để iem pùn ẹ đọc tới đây rùi thì bỏ qua nhoaaa <3 .',
+    btnAccept: 'Okiiiii lun em tha choaaa anhhh <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100017247575335}
